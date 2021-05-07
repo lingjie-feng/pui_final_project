061659f04a9f594ca8b30f0052cc66ad3fa2e545
lingjie-feng <flj@Lingjies-MBP.fios-router.home>
2021-05-06 22:25:40 -0400

    finalize navigation bar

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ home page grid: https://www.w3schools.com/bootstrap/bootstrap_templates.asp
 door image transition animation: https://www.youtube.com/watch?v=V8eAyZjfuZc
 typewriter: https://codepen.io/daviddcarr/pen/XVyQMM
 hover effect: https://miketricking.github.io/bootstrap-image-hover/#
+envelope animation: https://codepen.io/adamgen/pen/PNPxag
--- static/js/main.js
+++ static/js/main.js
@@ -101,29 +101,3 @@ function typeWriter(id, ar, i, a, isBackspacing, isParagraph, speedForward, spee
     }
   }
 }
-
-
-/* Home page functions */
-function homePageActivate() {
-  $(function () {
-    $(".navigation-tab-item").click(function () {
-        $(".navigation-tab-item").removeClass("active");
-        $(this).addClass("active");
-        $(".navigation-tab-overlay").css({
-            left: $(this).prevAll().length * 195 + "px"
-        });
-    });
-  });
-  $('#nav_container').mouseenter(function () {
-    window.addEventListener('wheel', scrollevent);                 
-  });
-  $('#nav_container').mouseleave(function () {
-    window.removeEventListener('wheel', scrollevent);
-  });
-}
-
-function scrollevent(e) {
-  var navContainer = $('#nav_container')[0];
-  if (e.deltaY > 0) navContainer.scrollLeft += 100;
-  else navContainer.scrollLeft -= 100;
-}
